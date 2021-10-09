d0d19ead5c23975dbc8babf91a06ac4ca34842a2
Jacob Madsen <boogath@gmail.com>
2021-10-09 13:32:08 -0600

    Added additional Print Statement.

--- src/Cristeche/Main.java
+++ src/Cristeche/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hey, this is Cristeche");
+
+        System.out.println("Hello Cristeche, hope your well!");
     }
 }
